aecb68504c5a7dbd737a02cc21937c77251d1f3b
Váradi, Róbert (U105976) <robert.varadi.sp@lhsystems.com>
2025-04-08 15:05:17 +0200

    add repro code

---
+++ liquibase-classloader-issue-repro/src/main/java/Repro.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import liquibase.Scope;
+
+public class Repro {
+
+  public Repro() {
+  }
+
+  public void doRepro() throws InterruptedException {
+    ExecutorService executorService = Executors.newFixedThreadPool(1);
+    executorService.submit(() -> {
+      System.out.println("init thread");
+    });
+
+    Thread.sleep(100);
+
+    try {
+      File liquibaseJar = new File("lib/liquibase-core.jar");
+      URLClassLoader customLoader = new URLClassLoader(
+          new URL[]{liquibaseJar.toURI().toURL()},
+          null // No parent
+      );
+
+      executorService.submit(() -> {
+        Thread.currentThread().setContextClassLoader(customLoader);
+        Scope.getCurrentScope();
+      });
+      Scope.getCurrentScope();
+
+    } catch (MalformedURLException e) {
+      throw new RuntimeException(e);
+    } finally {
+      executorService.shutdown();
+    }
+  }
+}
